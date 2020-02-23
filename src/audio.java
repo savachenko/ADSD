75368d809050dccca1fbe044697a446f9fdcc4cb
alena <alenasavachenko3@gmail.com>
2020-02-23 16:43:15 +0100

    audio play ringtone

--- src/audio.java
+++ src/audio.java
@@ -11,52 +11,6 @@ public class audio{
 
     }
 
-    public static synchronized void playSound(final String url) {
-
-        System.out.println("void play sound");
-
-        new Thread(new Runnable() {
-
-            public void run() {
-
-                System.out.println("void run");
-
-                try {
-                    File soundFile = new File("C:\\Users\\Used ProductsDownloads\\\\\ringtone.wav"); //Звуковой файл
-
-                    //Получаем AudioInputStream
-                    //Вот тут могут полететь IOException и UnsupportedAudioFileException
-                    AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
-
-                    //Получаем реализацию интерфейса Clip
-                    //Может выкинуть LineUnavailableException
-                    Clip clip = AudioSystem.getClip();
-
-                    //Загружаем наш звуковой поток в Clip
-                    //Может выкинуть IOException и LineUnavailableException
-                    clip.open(ais);
-
-                    clip.setFramePosition(1); //устанавливаем указатель на старт
-                    clip.start(); //Поехали!!!
-
-                    //Если не запущено других потоков, то стоит подождать, пока клип не закончится
-                    //В GUI-приложениях следующие 3 строчки не понадобятся
-                    Thread.sleep(clip.getMicrosecondLength() / 1000);
-                    clip.stop(); //Останавливаем
-                    clip.close(); //Закрываем
-                } catch (IOException | UnsupportedAudioFileException | LineUnavailableException exc) {
-                    exc.printStackTrace();
-                } catch (InterruptedException exc) {
-                }
-
-            }
-
-
-
-        });
-
-
-    }
 }
 
 
@@ -64,10 +18,10 @@ public class audio{
 class Mythread extends Thread {
     public void run() {
 
-        System.out.println("void run");
+
 
         try {
-            File soundFile = new File("C:\\Users\\Used Products\\Downloads\\ringtone.wav"); //Звуковой файл
+            File soundFile = new File("alarm1.wav"); //Звуковой файл
 
             //Получаем AudioInputStream
             //Вот тут могут полететь IOException и UnsupportedAudioFileException
@@ -86,7 +40,8 @@ class Mythread extends Thread {
 
             //Если не запущено других потоков, то стоит подождать, пока клип не закончится
             //В GUI-приложениях следующие 3 строчки не понадобятся
-            Thread.sleep(clip.getMicrosecondLength() / 1000);
+            //Thread.sleep(clip.getMicrosecondLength() /60);
+            Thread.sleep(7000);
             clip.stop(); //Останавливаем
             clip.close(); //Закрываем
         } catch (IOException | UnsupportedAudioFileException | LineUnavailableException exc) {
